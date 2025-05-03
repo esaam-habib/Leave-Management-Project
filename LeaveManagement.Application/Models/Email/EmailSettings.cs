6fb433ebb6e7ddf8a7dd1a160914a9a4a7906a73
esaam-habib <esaamhabib2000@gmail.com>
2025-05-03 17:05:22 +0530

    Added models for the leave application

--- LeaveManagement.Application/Models/Email/EmailSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HR.LeaveManagement.Application.Models.Email
-{
-    public class EmailSettings
-    {
-        public string ApiKey { get; set; }
-        public string FromAddress { get; set; }
-        public string FromName { get; set; }
-    }
-}
--- LeaveManagement.Application/Models/Identity/RegistrationResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HR.LeaveManagement.Application.Models.Identity
-{
-    public class RegistrationResponse
-    {
-        public string UserId { get; set; }
-    }
-}
